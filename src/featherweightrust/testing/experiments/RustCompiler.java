8d5535acada575450582c089e28243dd76d9865d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2020-06-23 20:14:38 +1200

    Update Rustc Compiler flags
    This puts through a compiler flag for "--emit mir" which actually reduces
    compile time by preventing the creation of an actual object file.

--- src/featherweightrust/testing/experiments/RustCompiler.java
+++ src/featherweightrust/testing/experiments/RustCompiler.java
@@ -77,8 +77,8 @@ public final class RustCompiler {
 		// ===================================================
 		ArrayList<String> command = new ArrayList<>();
 		command.add(rust_cmd);
-		command.add("-C");
-		command.add("opt-level=0");
+		command.add("--emit");
+		command.add("mir");
 		command.add("-A");
 		command.add("unused-assignments");
 		command.add("-A");
